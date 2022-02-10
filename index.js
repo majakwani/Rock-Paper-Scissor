8527dd31d3b8a5ee8f8865cbcb81b7a799eb8480
Anas Jakwani <anasjakwani72@gmail.com>
2022-02-11 03:01:40 +0500

    Added UI to the game and used DOM to make game more interactive for user

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
+let userNameInput = prompt("Enter your name?");
+let  userName = document.querySelector('.user');
+userName.innerHTML = userNameInput + " :  ";
+
+let rockBtn = document.querySelector('.rock');
+let paperBtn = document.querySelector('.paper');
+let scissorBtn = document.querySelector('.scissor');
+let resultDisplay = document.querySelector('.v2h1');
+let userCounter = 0, computerCounter = 0;
+let userScore = document.querySelector('.userscoredisplay');
+let computerScore = document.querySelector('.computerscore');
+let child = document.querySelector('.vertical2');
+let link = document.createElement('div');
+link.setAttribute("class", "v2h2")
+link.innerHTML = `<a href="./index.html">Play Again</a>`
+
+function userInput(number){
+     switch(number){
+          case 1:
+               return rockBtn.value;
+               break;
+
+          case 2:
+               return paperBtn.value;
+               break;
+          
+          case 3:
+               return scissorBtn.value;
+               break;
+          
+          default:
+               break;
+     }
+}
+
 function computerPlay(){
      let number = Math.floor(Math.random() * 3)
      switch(number){
@@ -22,46 +57,46 @@ function playRound(playerSelection, computerSelection){
      switch (playerSelection){
           case "ROCK":
                if (computerSelection == "ROCK" ){
-                    console.log ("You both selected Rock, It's a draw")
-                    return "DRAW"
+                    resultDisplay.innerHTML = "<p>You both selected Rock, It's a draw</p>";
+                    counter("DRAW");
                }
                if (computerSelection == "PAPER"){
-                    console.log("Computer selected Paper, You lost")
-                    return "COMPUTER"
+                    resultDisplay.innerHTML = "<p>Computer selected Paper, You lost</p>";
+                    counter("COMPUTER");
                }
                if (computerSelection == "SCISSOR"){
-                    console.log("Computer selected Scissor, You won")
-                    return "USER"
+                    resultDisplay.innerHTML = "<p>Computer selected Scissor, You won</p>";
+                    counter("USER");
                }
                break;
 
                case "PAPER":
                if (computerSelection == "ROCK" ){
-                    console.log ("Computer Selected Rock, You won")
-                    return "USER"
+                    resultDisplay.innerHTML = "<p>Computer Selected Rock, You won</p>";
+                    counter("USER");
                }
                if (computerSelection == "PAPER"){
-                    console.log("Yoy both selected Paper, It's a draw")
-                    return "DRAW"
+                    resultDisplay.innerHTML = "<p>Yoy both selected Paper, It's a draw</p>";
+                    counter("DRAW");
                }
                if (computerSelection == "SCISSOR"){
-                    console.log("Computer selected Scissor, You lost")
-                    return "COMPUTER"
+                    resultDisplay.innerHTML = "<p>Computer selected Scissor, You lost</p>";
+                    counter("COMPUTER");
                }
                break;
 
                case "SCISSOR":
                if (computerSelection == "ROCK" ){
-                    console.log ("Computer Selected Rock, You lost")
-                    return "COMPUTER"
+                    resultDisplay.innerHTML = "<p>Computer Selected Rock, You lost</p>";
+                    counter("COMPUTER");
                }
                if (computerSelection == "PAPER"){
-                    console.log("Computer selected Paper, You won")
-                    return "USER"
+                    resultDisplay.innerHTML = "<p>Computer selected Paper, You won</p>";
+                    counter("USER");
                }
                if (computerSelection == "SCISSOR"){
-                    console.log("You both selected Scissor, It's a draw")
-                    return "DRAW"
+                    resultDisplay.innerHTML = "<p>You both selected Scissor, It's a draw</p>";
+                    counter("DRAW");
                }
                break;
 
@@ -70,36 +105,33 @@ function playRound(playerSelection, computerSelection){
      }
 }
 
-function game(){
-     let userCounter = 0;
-     let computerCounter = 0;
-     for(i = 0; i < 9; i++ ){
-          let userinput = prompt("Select Rock, Paper or Scissor");
-          userinput = userinput.toUpperCase();
-         let result =  playRound(userinput, computerPlay());
-         
-         if(result == "DRAW"){
-              result = "";
-              i = i - 1;
-              continue;
-         }
-         else if(result == "USER"){
-              result = "";
-              userCounter = userCounter + 1;
-         }
-         else if (result == "COMPUTER"){
-              result = ""
-              computerCounter = computerCounter + 1;
-         }
-         if(userCounter == 5){
-          console.log("Congratulations, you won the game")
-          break;
-     }
-     else if (computerCounter == 5){
-          console.log("Bad Luck, You lost. Try Again.")
-          break;
-     }
-     }
-}
+function counter(result){
+     
+     switch(result){
+          case "USER":
+               userCounter += 1;
+               userScore.innerHTML = userCounter;
+               computerScore.innerHTML = computerCounter;
+               
+               if(userCounter == 5){
+                    resultDisplay.innerHTML = `Congratulations, You won the game.`;
+                    child.appendChild(link);
+               }
+               
+               break;
+          
+          case "COMPUTER":
+               computerCounter += 1;
+               userScore.innerHTML = userCounter;
+               computerScore.innerHTML = computerCounter;
+               
+               if(computerCounter == 5){
+                    resultDisplay.innerHTML = `Computer won the game, Hard Luck.`;
+                    child.appendChild(link);
+               }
+               
+               break;
 
-game();
+     }
+
+}
